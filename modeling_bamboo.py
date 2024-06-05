d7b13ae90fc92659b9bfc83abdc3cf1addba2540
Yixin Song <474309045@qq.com>
2024-06-05 14:16:57 +0800

    update

--- modeling_bamboo.py
+++ modeling_bamboo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f554cc42fd2079953a3800b60929be2d7acc780ca7cb92665846b02fb75651
-size 86637
+oid sha256:72ce0cac56f1c221d65858511a4ba868be3064fd24b5518ce11e58b59922f76f
+size 86680
